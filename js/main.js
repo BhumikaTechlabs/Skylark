1e0b19f6d75f50d70c92ed52c078ca28af6066a7
Bhumika Saini <bs98.saini@gmail.com>
2018-09-17 02:13:34 +0530

    some formatting

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ document.getElementById("map").style.height = window.innerHeight + "px";
 // When the document is ready
 $(document).ready(function ()
 {
-	$('.sidenav').sidenav();
+	//$('.sidenav').sidenav();
 	$('select').formSelect();
 	updateUI();
 });
@@ -36,151 +36,148 @@ function updateUI()
 	db.ref('/' + year).once('value').then(function (snapshot)
 	{
 		// ******* Left panel updates *******
-        
-        // (1) Host section update
-        countryData = snapshot.val().host_code;
-        containerName = "hostContainer";
-        containerTitle = "Host";
-        document.getElementById(containerName).innerHTML = "<h5>"+containerTitle+"</h5>";
+
+		// (1) Host section update
+		countryData = snapshot.val().host_code;
+		containerName = "hostContainer";
+		containerTitle = "Host";
+		document.getElementById(containerName).innerHTML = "<div style='background: #00FF00; width: 20px; height: 20px; display: inline-block; margin-right: 5px'></div><h5 style='display: inline-block'>Host</h5><br/>";
 		if (Array.isArray(countryData))
 		{
-            for (i = 1; i < countryData.length; i++) {
-                    document.getElementById(containerName).innerHTML += '<img src="flags/4x3/' + countryData[i] + '.svg" style="width:40px; height: 25px; display: inline-block; margin-right: 5px;"/>';
-                    console.log("Host ---> "+countryData[i]);
-                }
-        }
+			for (i = 1; i < countryData.length; i++)
+			{
+				document.getElementById(containerName).innerHTML += '<img src="flags/4x3/' + countryData[i] + '.svg" style="width:40px; height: 25px; display: inline-block; margin-right: 5px;"/>';
+				console.log("Host ---> " + countryData[i]);
+			}
+		}
 		else
 		{
-            document.getElementById(containerName).innerHTML += '<img src="flags/4x3/' + countryData + '.svg" style="width:40px; height: 25px; display: inline-block; margin-right: 5px;"/>';
-            console.log("Host ---> "+countryData);
-        }
-        
+			document.getElementById(containerName).innerHTML += '<img src="flags/4x3/' + countryData + '.svg" style="width:40px; height: 25px; display: inline-block; margin-right: 5px;"/>';
+			console.log("Host ---> " + countryData);
+		}
+
 		// (2) Champion section update
 		countryName = snapshot.val().champion_code;
-        containerName = "championContainer";
-        containerTitle = "Champion";
-		document.getElementById(containerName).innerHTML = "<h5>"+containerTitle+"</h5>";
-        document.getElementById(containerName).innerHTML += '<img src="flags/4x3/' + countryName + '.svg" style="width:40px; height: 25px; display: inline-block; margin-right: 5px;"/>';
-		console.log("Champion ---> "+countryName);
-        
-        // ******* Map panel update *******
+		containerName = "championContainer";
+		containerTitle = "Champion";
+		document.getElementById(containerName).innerHTML = "<div style='background: #FF0000; width: 20px; height: 20px; display: inline-block; margin-right: 5px'></div><h5 style='display: inline-block'>Champion</h5><br/>";
+		document.getElementById(containerName).innerHTML += '<img src="flags/4x3/' + countryName + '.svg" style="width:40px; height: 25px; display: inline-block; margin-right: 5px;"/>';
+		console.log("Champion ---> " + countryName);
+
+		// ******* Map panel update *******
 		var mapboxClient = mapboxSdk(
 		{
 			accessToken: mapboxgl.accessToken
 		});
-        
-        // clear the map
-        if (hostMarker.length>0)
-        {
-            for(i=0; i<hostMarker.length; i++)
-                hostMarker[i].remove();
-            hostMarker = [];
-        }
-        
-        // add host markers
-        countryName = snapshot.val().host;
-        if(!Array.isArray(countryName))
-		  updateMap(mapboxClient, countryName);
-        else {
-            for (i=1; i<countryName.length; i++)
-               updateMap(mapboxClient, countryName[i]);
-        }
-        // add champion markers
-        countryName = snapshot.val().champion;
-        if(!Array.isArray(countryName))
-		  updateMap(mapboxClient, countryName);
-        else {
-            for (i=1; i<countryName.length; i++)
-               updateMap(mapboxClient, countryName[i]);
-        }
-        // add participant markers
-        countryName = snapshot.val().host;
-        if(!Array.isArray(countryName))
-		  updateMap(mapboxClient, countryName);
-        else {
-            for (i=1; i<countryName.length; i++)
-               updateMap(mapboxClient, countryName[i]);
-        }
+
+		// clear the map
+		if (hostMarker.length > 0)
+		{
+			for (i = 0; i < hostMarker.length; i++)
+				hostMarker[i].remove();
+			hostMarker = [];
+		}
+
+		// add host markers
+		countryName = snapshot.val().host;
+		if (!Array.isArray(countryName))
+			updateMap(mapboxClient, countryName, 0, 255, 0);
+		else
+		{
+			for (i = 1; i < countryName.length; i++)
+				updateMap(mapboxClient, countryName[i], 0, 255, 0);
+		}
+		// add champion markers
+		countryName = snapshot.val().champion;
+		if (!Array.isArray(countryName))
+			updateMap(mapboxClient, countryName, 255, 0, 0);
+		else
+		{
+			for (i = 1; i < countryName.length; i++)
+				updateMap(mapboxClient, countryName[i], 255, 0, 0);
+		}
+		// add participant markers
+		countryName = snapshot.val().participant;
+		if (!Array.isArray(countryName))
+			updateMap(mapboxClient, countryName, 0, 0, 255);
+		else
+		{
+			for (i = 1; i < countryName.length; i++)
+				updateMap(mapboxClient, countryName[i], 0, 0, 255);
+		}
 	});
 }
 
-function updateMap(mapboxClient, country) {
-    mapboxClient.geocoding.forwardGeocode(
+function updateMap(mapboxClient, country, r, g, b)
+{
+	mapboxClient.geocoding.forwardGeocode(
+		{
+			query: country,
+			autocomplete: false,
+			limit: 1
+		})
+		.send()
+		.then(function (response)
+		{
+			if (response && response.body && response.body.features && response.body.features.length && count == 0)
 			{
-				query: country,
-				autocomplete: false,
-				limit: 1
-			})
-			.send()
-			.then(function (response)
+				++count;
+				feature = response.body.features[0];
+				map = new mapboxgl.Map(
+				{
+					container: 'map',
+					style: 'mapbox://styles/mapbox/dark-v9',
+					center: feature.center,
+					zoom: 2.0,
+					maxBounds: [
+						[-180, -90],
+						[180, 90]
+					]
+				});
+
+				// add geocoder
+				map.addControl(new MapboxGeocoder(
+				{
+					accessToken: mapboxgl.accessToken
+				}));
+
+				// add nav control
+				map.addControl(new mapboxgl.NavigationControl());
+
+				// add curr loc button
+				map.addControl(new mapboxgl.GeolocateControl(
+				{
+					positionOptions:
+					{
+						enableHighAccuracy: true
+					},
+					trackUserLocation: true
+				}));
+			}
+			else if (response && response.body && response.body.features && response.body.features.length && count > 0)
 			{
-				if (response && response.body && response.body.features && response.body.features.length && count == 0)
+				feature = response.body.features[0];
+			}
+
+			// create the popup
+			var popup = new mapboxgl.Popup(
 				{
-                    ++count;
-					feature = response.body.features[0];
-					map = new mapboxgl.Map(
-					{
-						container: 'map',
-						style: 'mapbox://styles/mapbox/dark-v9',
-						center: feature.center,
-						zoom: 3.0,
-						maxBounds: [
-							[-180, -90],
-							[180, 90]
-						]
-					});
-                    
-                    // add geocoder
-					map.addControl(new MapboxGeocoder(
-					{
-						accessToken: mapboxgl.accessToken
-					}));
-                    
-                    // add nav control
-                    map.addControl(new mapboxgl.NavigationControl());
-                    
-                    // add curr loc button
-                    map.addControl(new mapboxgl.GeolocateControl({
-                        positionOptions: {
-                            enableHighAccuracy: true
-                        },
-                        trackUserLocation: true
-                    }));
-                    
-                    // create the popup
-                    var popup = new mapboxgl.Popup({ offset: 25 })
-                        .setText(country);
+					offset: 25
+				})
+				.setText(country);
 
-                    // create DOM element for the marker
-                    var el = document.createElement('div');
-                    el.id = 'marker';
-                    
-					hostMarker.push(new mapboxgl.Marker()
-						.setLngLat(feature.center)
-                        .setPopup(popup)
-						.addTo(map));
-
-				}
-				else if (response && response.body && response.body.features && response.body.features.length && count > 0)
+			hostMarker.push(new mapboxgl.Marker(
 				{
-					feature = response.body.features[0];
-					//hostMarker.remove();
-					// create the popup
-                    var popup = new mapboxgl.Popup({ offset: 25 })
-                        .setText(country);
+					color: `rgb(` + r + `, ` + g + `, ` + b + `)`
+				})
+				.setLngLat(feature.center)
+				.setPopup(popup)
+				.addTo(map));
 
-                    // create DOM element for the marker
-                    var el = document.createElement('div');
-                    el.id = 'marker';
-                    
-					hostMarker.push(new mapboxgl.Marker()
-						.setLngLat(feature.center)
-                        .setPopup(popup)
-						.addTo(map));
-					
-                    map.flyTo({
-                        center: feature.center
-                    });
-				}
+			map.flyTo(
+			{
+				center: feature.center
 			});
+
+		});
 }
